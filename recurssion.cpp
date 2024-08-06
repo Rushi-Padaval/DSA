f2288a1da5296f712a611c5d0972209ab57c8457
Rushikesh <143233718+Rushi-Padaval@users.noreply.github.com>
2024-08-06 18:58:55 +0530

    Add files via upload

---
+++ recurssion.cpp
@@ -0,0 +1,30 @@
+
+
+//recursion 
+// a function call itself until a specified mentioned 
+
+//stack overflow 
+// recursion ends with the overflow  
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int cnt = 0 ;
+
+void print(){
+ 
+      if (cnt == 3) return ;
+      cout << cnt << endl;
+       cnt ++;
+      print();
+
+}
+
+int main()
+{
+    print();
+ return 0;
+}
